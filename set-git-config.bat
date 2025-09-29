09e97cc4b04fa156f55249ae7839197cb4ff398a
Euri Villanueva <eurimiguel.villanueva@cvsu.edu.ph>
2025-09-29 01:15:23 -0700

    Delete set-git-config.bat

--- set-git-config.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-:: Set your Git global configuration
-git config --global user.name "Your Name"
-git config --global user.email "your.email@example.com"
-
-echo Git configuration has been set!
-pause
