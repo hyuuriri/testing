b718ed9349bd8f5d3de1885cb46ba3ea62d4b793
Euri Villanueva <eurimiguel.villanueva@cvsu.edu.ph>
2025-09-29 01:17:37 -0700

    Add files via upload

---
+++ set-git-config.bat
@@ -0,0 +1,7 @@
+@echo off
+:: Set your Git global configuration
+git config --global user.name "hyuuriri"
+git config --global user.email "eurimiguel.villanueva@cvsu.edu.ph"
+
+echo Git configuration has been set!
+pause
